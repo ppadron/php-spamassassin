6ff6c310925c0242aab6b57061a44713fe7757a5
Pedro Padron <ppadron@w3p.com.br>
2010-04-14 08:04:10 -0300

    initial commit

---
+++ SpamAssassin/Client.php
@@ -0,0 +1,90 @@
+<?php
+
+class SpamAssassin_Client
+{
+
+    const PROCESS = 'PROCESS';
+    const CHECK   = 'CHECK';
+
+    public function __construct($hostname = 'localhost', $port = '783')
+    {
+        $this->socket = socket_create(AF_INET, SOCK_STREAM, getprotobyname("tcp"));
+        socket_connect($this->socket, $hostname, $port);
+    }
+
+    private function _exec($cmd)
+    {
+        $this->_write($cmd, false);
+        return $this->_read();
+    }
+
+    private function _write($data)
+    {
+        socket_write($this->socket, $data);
+        socket_shutdown($this->socket, 1);
+    }
+
+    private function _read()
+    {
+        $return = '';
+
+        do {
+            $buffer = socket_read($this->socket, 128);
+            $return .= $buffer;
+        } while ($buffer != "");
+
+        return $return;
+    }
+
+    public function ping()
+    {
+
+        $return = $this->_exec("PING SPAMC/1.3\n");
+
+        if (strpos($return, "PONG") == false) {
+            return false;
+        }
+
+        return true;
+
+    }
+
+    public function process($message, $processMethod = self::CHECK)
+    {
+        $lenght = strlen($message . "\n");
+
+        $cmd  = "CHECK " . "SPAMC/1.2\r\n";
+        $cmd .= "Content-lenght: $lenght\r\n";
+        $cmd .= "\r\n";
+        $cmd .= $message;
+        $cmd .= "\r\n";
+
+        $output = $this->_exec($cmd);
+
+        $lines = explode("\r\n", $output);
+
+        $matches = array();
+
+        preg_match(
+            '/^Spam: (True|False) ; (\S+) \/ (\S+)/',
+            $lines[1],
+            &$matches
+        );
+
+        if (empty($matches)) {
+            throw new SpamAssassin_Exception("Could not parse response for $processMethod command");
+        }
+
+        $result = array();
+
+        ($matches[1] == 'True') ?
+            $result['is_spam'] = true :
+            $result['is_spam'] = false;
+
+        $result['score']    = (float) $matches[2];
+        $result['thresold'] = (float) $matches[3];
+
+        return $result;
+    }
+
+}
---
+++ tests/TestBasicCommunication.php
@@ -0,0 +1,39 @@
+<?php
+
+require_once 'PHPUnit/Framework/TestCase.php';
+require_once 'SpamAssassin/Client.php';
+
+class BasicCommunication extends PHPUnit_Framework_TestCase
+{
+    public function setUp()
+    {
+        $this->sa = new SpamAssassin_Client("localhost", "783");
+    }
+
+    public function testPing()
+    {
+        $this->assertEquals(true, $this->sa->ping());
+    }
+
+    public function testProcessSpamMessage()
+    {
+        $message = file_get_contents(dirname(__FILE__) . "/files/spam.txt");
+        $return = $this->sa->process($message);
+
+        $this->assertEquals(true,   is_array($return));
+        $this->assertEquals(true,   $return['is_spam']);
+        $this->assertEquals(5.0,    $return['thresold']);
+        $this->assertEquals(1000.0, $return['score']);
+    }
+
+    public function testProcessHamMessage()
+    {
+        $message = file_get_contents(dirname(__FILE__) . "/files/ham.txt");
+        $return = $this->sa->process($message);
+
+        $this->assertEquals(true,  is_array($return));
+        $this->assertEquals(false, $return['is_spam']);
+        $this->assertEquals(5.0,   $return['thresold']);
+        $this->assertEquals(0,     $return['score']);
+    }
+}
