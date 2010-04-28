629a5f4f6ff168c4ba17e033071464a3b9758ce6
Pedro Padron <ppadron@w3p.com.br>
2010-04-28 18:26:50 -0300

    declared some vars in SpamAssassin_Client_Result

--- SpamAssassin/Client/Result.php
+++ SpamAssassin/Client/Result.php
@@ -1,3 +1,20 @@
 <?php
 
-class SpamAssassin_Client_Result { }
+class SpamAssassin_Client_Result {
+
+    public $responseCode;
+
+    public $responseMessage;
+
+    public $contentLenght;
+
+    public $score;
+
+    public $thresold;
+
+    public $isSpam;
+
+    public $output;
+
+
+}
