ebebeb3aab2a7317479a1764f618e6868957fe99
Pedro Padron <ppadron@w3p.com.br>
2010-10-20 11:48:01 -0200

    ReportTest and RevokeTest should skip if TELL command is disabled on the server

--- tests/ReportTest.php
+++ tests/ReportTest.php
@@ -4,6 +4,20 @@ require_once 'BaseTestCase.php';
 
 class ReportTest extends BaseTestCase
 {
+    public function setUp()
+    {
+        parent::setUp();
+        
+        if (!isset($GLOBALS['PHPUNIT_SA_LEARN_ENABLED'])
+            || $GLOBALS['PHPUNIT_SA_LEARN_ENABLED'] == 0) {
+
+            $this->markTestSkipped(
+                'To test the learning behavior, enable the TELL ' .
+                'command in SpamAssassin and update phpunit.xml'
+            );
+        }
+    }
+
     public function testShouldReportMessageAsSpam()
     {
         // the message cannot be older than 2 days
