d1bb3b41e1d62421d2865e1a8e5dc1e2ae132401
Konstantin Pereyaslov <perk11@perk11.info>
2015-12-09 09:20:11 +0300

    Extend \Exception class instead of itself in Client\Exception

--- src/Spamassassin/Client/Exception.php
+++ src/Spamassassin/Client/Exception.php
@@ -8,7 +8,7 @@ namespace Spamassassin\Client;
  * @author   Pedro Padron <ppadron@w3p.com.br>
  * @license  http://www.apache.org/licenses/LICENSE-2.0.html Apache License 2.0
  */
-class Exception extends Exception
+class Exception extends \Exception
 {
 
 }
